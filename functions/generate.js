ee377276c0200e23d01cfc74ba793e7564ab65d8
tanvirsubeh-creator <tanvirsubeh@gmail.com>
2026-03-31 15:06:03 -0400

    Update generate.js

--- functions/generate.js
+++ functions/generate.js
@@ -29,7 +29,7 @@ exports.handler = async (event) => {
         "content-type": "application/json"
       },
       body: JSON.stringify({
-        model: "claude-3-haiku-20240307",
+        model: "claude-haiku-4-5-20251001",
         max_tokens: 500,
         messages: [{
           role: "user",
