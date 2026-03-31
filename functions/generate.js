a78343a27aa4951bbc1634a3ad20e57b983d4e4c
tanvirsubeh-creator <tanvirsubeh@gmail.com>
2026-03-31 15:12:32 -0400

    Update generate.js

--- functions/generate.js
+++ functions/generate.js
@@ -86,7 +86,7 @@ exports.handler = async (event) => {
         },
         body: JSON.stringify({
           text: story.script,
-          model_id: "eleven_monolingual_v1",
+          model_id: "eleven_turbo_v2_5",
           voice_settings: {
             stability: 0.5,
             similarity_boost: 0.75
